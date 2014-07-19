ef4f7c0ef3b8ac2b13384ce45a91906d62a65533
William Blankenship <william.jblankenship@gmail.com>
2014-07-19 14:02:07 -0500

    Now using gnu parallel to execute tasks

--- build.sh
+++ build.sh
@@ -4,20 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Logs matter.
 mkdir -p $DIR/logs/
 
-# Build Debian Containers.
-docker build -t crackerz/nodejs:jessie $DIR/debian/jessie/ 1> ./logs/jessie.log 2>&1 &
-docker build -t crackerz/nodejs:sid $DIR/debian/sid 1> ./logs/sid.log 2>&1 &
-docker build -t crackerz/nodejs:wheezy $DIR/debian/wheezy 1> ./logs/wheezy.log 2>&1 &
+# Build Containers.
+parallel "docker build -t crackerz/nodejs:{} $DIR/dist/{} 2>&1 | tee ./logs/{}.log" ::: wheezy jessie sid precise saucy trusty
 
-# Build Ubuntu Containers.
-docker build -t crackerz/nodejs:precise $DIR/ubuntu/precise 1> ./logs/precise.log 2>&1 &
-docker build -t crackerz/nodejs:saucy $DIR/ubuntu/saucy 1> ./logs/saucy.log 2>&1 &
-docker build -t crackerz/nodejs:trusty $DIR/ubuntu/trusty 1> ./logs/trusty.log 2>&1 &
-
-# Output Progress to Terminal (comment out in production)
-tail  -f ./logs/jessie.log \
-      -f ./logs/sid.log \
-      -f ./logs/wheezy.log \
-      -f ./logs/precise.log \
-      -f ./logs/saucy.log \
-      -f ./logs/trusty.log
+docker push crackerz/nodejs 2>&1 | tee ./logs/push.log
