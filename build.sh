13e202f26ab941d5f2cb72df7814b137864c3d57
William Blankenship <william.jblankenship@gmail.com>
2014-07-19 14:08:16 -0500

    Logs belong in cwd

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Logs matter.
-mkdir -p $DIR/logs/
+mkdir -p ./logs/
 
 # Build Containers.
-parallel "docker build -t crackerz/nodejs:{} $DIR/dist/{} 2>&1 | tee ./logs/{}.log" ::: wheezy jessie sid precise saucy trusty
+parallel "docker build --no-cache -t crackerz/nodejs:{} $DIR/dist/{} 2>&1 | tee ./logs/{}.log" ::: wheezy jessie sid precise saucy trusty
 
 docker push crackerz/nodejs 2>&1 | tee ./logs/push.log
